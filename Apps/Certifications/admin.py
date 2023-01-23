f1e44d4dde70d59aec1053d0dbd040f2f106e3ea
Alejandro Acho <61828943+Alejandroacho@users.noreply.github.com>
2023-01-23 23:21:08 +0100

    feat: Certification model added to admin

--- Apps/Certifications/admin.py
+++ Apps/Certifications/admin.py
@@ -1,4 +1,36 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+
+from Certifications.models import Certification
 
 
-# Register your models here.
+class CertificationAdmin(ModelAdmin):
+    list_display: tuple = ("id", "name", "tags", "url")
+    list_display_links: tuple = (
+        "id",
+        "name",
+    )
+    fieldsets: tuple = (
+        (
+            "Overview",
+            {
+                "fields": (
+                    "id",
+                    "name",
+                    "description",
+                    "tags",
+                    "url",
+                    "file",
+                    "image",
+                )
+            },
+        ),
+    )
+    readonly_fields: list = [
+        "id",
+    ]
+    search_fields: tuple = ("name", "tags", "url", "id")
+    ordering: tuple = ("tags",)
+
+
+admin.site.register(Certification, CertificationAdmin)
