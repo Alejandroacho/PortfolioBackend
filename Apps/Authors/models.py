8b5f21cb2aa52db3ed5c3c679ccebbcfadd5e918
Alejandro Acho <61828943+Alejandroacho@users.noreply.github.com>
2023-02-20 19:40:41 +0100

    feat: Authors model created

--- Apps/Authors/models.py
+++ Apps/Authors/models.py
@@ -1,4 +1,21 @@
-from django.db import models
+from django.db.models import CharField
+from django.db.models import ManyToManyField
+from django.db.models import Model
+from django.db.models.fields import Field
+
+from SocialNetworks.models import SocialNetwork
 
 
-# Create your models here.
+class Author(Model):
+    first_name: Field = CharField(max_length=100, null=True)
+    last_name: Field = CharField(max_length=100, null=True)
+    social_networks: Field = ManyToManyField(
+        SocialNetwork, related_name="authors", null=True
+    )
+
+    def __str__(self) -> str:
+        return f"{self.id} | {self.first_name} {self.last_name}"
+
+    @property
+    def name(self) -> str:
+        return f"{self.first_name} {self.last_name}"
