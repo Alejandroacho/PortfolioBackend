49c02c39903e3fe0bfd44d4d488b8cb06eacf2e0
Alejandro Acho <61828943+Alejandroacho@users.noreply.github.com>
2023-02-24 23:49:45 +0100

    feat: Maintainers model added to project config

--- Apps/Maintainers/admin.py
+++ Apps/Maintainers/admin.py
@@ -1,4 +1,36 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+from django.db.models import Model
+
+from Maintainers.models import Maintainer
 
 
-# Register your models here.
+class MaintainerAdmin(ModelAdmin):
+    model: Model = Maintainer
+    list_display: tuple = ("id", "first_name", "last_name", "email")
+    list_display_links: tuple = ("id", "first_name")
+    list_filter: tuple = ("first_name", "last_name", "email")
+    fieldsets: tuple = (
+        (
+            "Overview",
+            {
+                "fields": (
+                    "id",
+                    "first_name",
+                    "last_name",
+                    "email",
+                    "about",
+                    "cv",
+                    "certifications",
+                    "images",
+                    "social_networks",
+                    "author",
+                )
+            },
+        ),
+    )
+    readonly_fields: list = ["id"]
+    search_fields: tuple = ("email", "last_name", "first_name", "id")
+
+
+admin.site.register(Maintainer, MaintainerAdmin)
--- Project/settings/default_django_settings.py
+++ Project/settings/default_django_settings.py
@@ -60,6 +60,7 @@ LOCAL_APPS: list = [
     "Certifications",
     "Authors",
     "Projects",
+    "Maintainers",
 ]
 
 INSTALLED_APPS: list = (
@@ -263,3 +264,7 @@ TWITTER_API_SECRET_KEY: str = None
 TWITTER_API_BEARER_TOKEN: str = None
 # OAUTH SETTINGS
 OAUTH_PASSWORD: str = None
+
+
+## Maintainers limit
+MAINTAINERS_LIMIT: int = 1
