60619ab79e7118736f90fb793543874c0c9e1abf
Alejandro Acho <61828943+Alejandroacho@users.noreply.github.com>
2023-03-01 22:25:36 +0100

    refactor: Projects views with new permission

--- Apps/Projects/tests/test_project_views.py
+++ Apps/Projects/tests/test_project_views.py
@@ -127,19 +127,80 @@ class TestRetrieveEndpoint:
         assert self.url(1) == "/api/projects/1/"
         assert self.url() == "/api/projects/"
 
-    def test_fails_as_unauthenticated(self) -> None:
+    def test_works_as_unauthenticated(self) -> None:
         project: Project = ProjectFaker()
         client: APIClient = APIClient()
         response: Response = client.get(self.url(project.id))
-        assert response.status_code == 401
+        assert response.status_code == 200
+        assert response.data["id"] == project.id
+        assert response.data["title"] == project.title
+        assert response.data["description"] == project.description
+        assert response.data["url"] == project.url
+        assert response.data["is_public"] == project.is_public
+        assert response.data["repository"] == project.repository
+        assert (
+            response.data["technologies"]
+            == TechnologySerializer(project.technologies.all(), many=True).data
+        )
+        assert (
+            response.data["authors"]
+            == AuthorSerializer(project.authors.all(), many=True).data
+        )
+        assert (
+            response.data["images"]
+            == ImageSerializer(project.images.all(), many=True).data
+        )
 
-    def test_fails_as_unverified(self) -> None:
+    def test_list_works_as_unauthenticated(self) -> None:
+        project: Project = ProjectFaker()
+        project2: Project = ProjectFaker()
+        client: APIClient = APIClient()
+        response: Response = client.get(self.url())
+        assert response.status_code == 200
+        assert response.data[1]["id"] == project.id
+        assert response.data[1]["title"] == project.title
+        assert response.data[1]["description"] == project.description
+        assert response.data[1]["url"] == project.url
+        assert response.data[1]["is_public"] == project.is_public
+        assert response.data[1]["repository"] == project.repository
+        assert (
+            response.data[1]["technologies"]
+            == TechnologySerializer(project.technologies.all(), many=True).data
+        )
+        assert (
+            response.data[1]["authors"]
+            == AuthorSerializer(project.authors.all(), many=True).data
+        )
+        assert (
+            response.data[1]["images"]
+            == ImageSerializer(project.images.all(), many=True).data
+        )
+        assert response.data[0]["id"] == project2.id
+        assert response.data[0]["title"] == project2.title
+        assert response.data[0]["description"] == project2.description
+        assert response.data[0]["url"] == project2.url
+        assert response.data[0]["is_public"] == project2.is_public
+        assert response.data[0]["repository"] == project2.repository
+        assert (
+            response.data[0]["technologies"]
+            == TechnologySerializer(project2.technologies.all(), many=True).data
+        )
+        assert (
+            response.data[0]["authors"]
+            == AuthorSerializer(project2.authors.all(), many=True).data
+        )
+        assert (
+            response.data[0]["images"]
+            == ImageSerializer(project2.images.all(), many=True).data
+        )
+
+    def test_works_as_unverified(self) -> None:
         project: Project = ProjectFaker()
         user: User = UserFaker()
         client: APIClient = APIClient()
         client.force_authenticate(user)
         response: Response = client.get(self.url(project.id))
-        assert response.status_code == 403
+        assert response.status_code == 200
 
     def test_retrieve_works_as_admin(self) -> None:
         project: Project = ProjectFaker()
