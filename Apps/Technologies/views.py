7c24ce0fe7f003b4b4a57a78862ecda2253abc35
Alejandro Acho <61828943+Alejandroacho@users.noreply.github.com>
2023-03-01 22:17:04 +0100

    refactor: Technologies views with new permission

--- Apps/Technologies/views.py
+++ Apps/Technologies/views.py
@@ -2,6 +2,7 @@ from django.db.models import QuerySet
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
+from Project.permissions import IsGetPetition
 from Technologies.models import Technology
 from Technologies.serializers import TechnologySerializer
 from Users.permissions import IsAdmin
@@ -12,4 +13,6 @@ class TechnologyViewSet(ModelViewSet):
     queryset: QuerySet = Technology.objects.all().order_by("-id")
     lookup_url_kwarg: str = "pk"
     serializer_class: TechnologySerializer = TechnologySerializer
-    permission_classes: list = [IsAuthenticated & IsVerified & IsAdmin]
+    permission_classes: list = [
+        (IsAuthenticated & IsVerified & IsAdmin) | IsGetPetition
+    ]
