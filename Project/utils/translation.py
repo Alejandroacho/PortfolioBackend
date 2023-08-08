328d73c96fb8f5a14a025e6a0966afb015ac337a
Alejandro Acho <alejandroacho@hotmail.com>
2023-08-08 17:58:52 +0200

    chore: Removed unnecessary things

--- Project/utils/translation.py
+++
@@ -1,7 +0,0 @@
-from django.utils.translation import gettext
-from django.utils.translation import override
-
-
-def get_translation_in(language: str, text: str) -> str:
-    with override(language):
-        return gettext(text)
