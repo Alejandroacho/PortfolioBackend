5297ddbee36850f52fcc5a957641fc466d0e282e
Alejandro Acho <alejandroacho@hotmail.com>
2023-08-10 22:39:29 +0200

    fix: Certifications module fully working

--- Apps/Certifications/views.py
+++ Apps/Certifications/views.py
@@ -1,4 +1,5 @@
 from django.db.models import QuerySet
+from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from Certifications.models import Certification
@@ -9,3 +10,4 @@ class CertificationViewSet(ReadOnlyModelViewSet):
     queryset: QuerySet = Certification.objects.all().order_by("-id")
     lookup_url_kwarg: str = "pk"
     serializer_class: CertificationSerializer = CertificationSerializer
+    permission_classes = [AllowAny]
